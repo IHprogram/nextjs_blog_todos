b3aa5170378174cf689b845199c6544761a25d16
IHprogram <ihm20202020@gmail.com>
2021-09-07 16:01:50 +0900

    フッターの作成

--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,15 @@ import Head from "next/head"
 export default function Layout({ children, title = "タイトル" }) {
   return (
     <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
-      <a>こんにちは</a>
-      <a>こんにちは2</a>
-      <a>こんにちは3</a>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-l justify-center items-center ws-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+        @Hiroyasu Iida
+      </footer>
     </div>
   )
 }
