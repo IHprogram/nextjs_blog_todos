45cb9921d939aba9ea1a8405897326f8d81ffa5b
IHprogram <ihm20202020@gmail.com>
2021-09-09 12:27:00 +0900

    Layoutコンポーネントでネスト

--- pages/main-page.js
+++ pages/main-page.js
@@ -12,9 +12,11 @@ const MainPage = () => {
   }
 
   return (
-    <svg onClick={logout} xmlns="http://www.w3.org/2000/svg" className="mt-10 cursor-pointer h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-    </svg>
+    <Layout title="Main Page">
+      <svg onClick={logout} xmlns="http://www.w3.org/2000/svg" className="mt-10 cursor-pointer h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+      </svg>
+    </Layout>
   )
 }
 
