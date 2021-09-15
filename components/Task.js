769f80e255206fb694d0ec3195520c1a6cfd41aa
IHprogram <ihm20202020@gmail.com>
2021-09-15 17:49:22 +0900

    タスク削除機能の実装

--- components/Task.js
+++ components/Task.js
@@ -1,4 +1,24 @@
-const Task = ({ task }) => {
+import { async } from "regenerator-runtime";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
+const Task = ({ task, taskDeleted }) => {
+  const deleteTask = async () => {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${cookie.get("access_token")}`,
+      }
+    }).then((res) => {
+      if (res.status === 401) {
+        alert("JWT Token not valid");
+      }
+    });
+    taskDeleted();
+  }
+
   return (
     <div>
       <span>{task.id}</span>
@@ -6,6 +26,9 @@ const Task = ({ task }) => {
       <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
         {task.title}
       </span>
+      <div className="float-right cursor-pointer">
+        <button type="button" onClick={deleteTask}>削除</button>
+      </div>
     </div>
   )
 }
--- pages/task-page.js
+++ pages/task-page.js
@@ -36,7 +36,7 @@ const TaskPage = ({ staticfilteredTasks }) => {
     <Layout title="Task Page">
       <ul>
         {
-          filteredTasks && filteredTasks.map((task) => <Task key={task.id} task={task} />)
+          filteredTasks && filteredTasks.map((task) => <Task key={task.id} task={task} taskDeleted={mutate} />)
         }
       </ul>
       <Link href="/main-page">
